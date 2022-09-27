b602eeef00fa01ec8931e8de7f9c7460e215ebd3
AlyonaShikaeva1 <71886949+AlyonaShikaeva1@users.noreply.github.com>
2022-09-28 02:07:00 +0300

    Add files via upload

--- Dentistry/db.php
+++ Dentistry/db.php
@@ -1,12 +1,18 @@
 <?php
 $servername = 'localhost';
 $username = 'root';
-$password = 'Vepfngs.ru1';
+$password = '12345';
 $db='my_dentistry';
 
 $connection = mysqli_connect($servername, $username, $password, $db);
 // Проверяем соединение
 if (!$connection) {
 die("Connection failed: " . mysqli_connect_error());
+}
+//врачи
+function get_doctors(){
+	global $connection;
+    $doctors=$connection->query("SELECT * FROM doctor");
+    return $doctors;
 }
 ?>
