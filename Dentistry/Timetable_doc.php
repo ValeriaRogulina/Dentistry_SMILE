33915feb87900807b8fbe432c548658655752ffa
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2022-11-25 19:51:44 +0300

    Add files via upload

--- Dentistry/Timetable_doc.php
+++ Dentistry/Timetable_doc.php
@@ -5,36 +5,28 @@ $result = get_doctor($_GET['id_doctor']);
 $time=get_times($_GET['id_doctor']);
 
 if(isset($_POST["zapis"])){
-  
-  if(!empty($_POST['date']) && !empty($_POST['time_reception']) ) {
- $date= $_POST['date'];
- $time_reception=$_POST['time_reception'];
- $doctor=$_POST['doctor'];
- $name_patient=$_SESSION['id'];
+    if(!empty($_POST['date']) && !empty($_POST['time_reception']) ) {
+        $date= $_POST['date'];
+        $time_reception=$_POST['time_reception'];
+        $doctor=$_POST['doctor'];
+        $name_patient=$_SESSION['id'];
 
- $query=$connection->query("SELECT * FROM reception WHERE 'date'='".$date."' AND time_reception='".$time_reception."'AND doctor='".$doctor."'");
-  $numrows=mysqli_num_rows($query);
-if($numrows==0)
-   {
-  $sqll="INSERT INTO reception (name_patient, 'date', time_reception, doctor) VALUES('$name_patient', '$date', '$time_reception', '$doctor')";
-  $res=$connection->query($sqll);
- if($res){
-  $message = "Вы записаны";
-} else {
- $message = "Не удалось записаться";
-  }
-  } else {
-  $message = "Это время занято";
-   }
-  } else {
-  $message = "Не выбраны все поля";
-  }
-  }
-  ?>
-
-
-
-
+    $query=$connection->query("SELECT * FROM reception WHERE 'date'='".$date."' AND time_reception='".$time_reception."'AND doctor='".$doctor."'");
+    $numrows=mysqli_num_rows($query);
+    if($numrows==0) {
+        $sqll="INSERT INTO reception (name_patient, 'date', time_reception, doctor) VALUES('$name_patient', '$date', '$time_reception', '$doctor')";
+        $res=$connection->query($sqll);
+    if($res){
+        $message = "Вы записаны";
+    } else {
+        $message = "Не удалось записаться";
+    } } else {
+        $message = "Это время занято";
+    } } else {
+        $message = "Не выбраны все поля";
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -48,8 +40,7 @@ if($numrows==0)
 	<link rel="stylesheet" href="libs/bootstrap-grid.min.css">
     <!-- Стили сайта -->
 	<link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" type="text/css" href="css/doctor_name.css">
-    <title><?php echo $result['name']; ?></title>
+    <title>Dentistry</title>
 </head>
 <body>
     <header>
@@ -76,77 +67,63 @@ if($numrows==0)
         </a>
     </div>                                                        
     </header>
-    
     <main>
         <div class="container">
             <div class="prices_title">
                 <h2>Запись к врачу онлайн</h2>
             </div>
-    <form action="Timetable_doc.php?id_doctor=<?php echo $result['id_doctor']; ?>" method="post">
-            <div class="header_profile">
-                <img src="<?php echo $result['photo']; ?>">
-            <h10>
-                         <?php echo "{$result['name']}"; ?>
-            </h10>
-
-                <h11><?php echo "{$result['speciality']}"; ?></h11>
-                <h11>Опыт работы: <?php echo "{$result['experience']}"; ?> лет</h11>
-             </div>
-
-            <div class="doctor_info">
-                <h12>Дата приёма</h12>
-                    <div class="row_schedule">
-                        <div class="row_schedule_btn">
-
-                    <?php
-                    $d = new DateTime(); ?>
-                        
-
-                            <input id="date-21.10.2022" type="radio" name="date" value="21.10.2022">
-                            <label for="date-21.10.2022"> <?php echo $d->modify('+1day')->format('d.m.Y'); ?></label>
+            <form action="Timetable_doc.php?id_doctor=<?php echo $result['id_doctor']; ?>" method="post">
+                <div class="header_profile">
+                    <img src="<?php echo $result['photo']; ?>">
+                    <h10><?php echo "{$result['name']}"; ?></h10>
+                    <h11><?php echo "{$result['speciality']}"; ?></h11>
+                    <h11>Опыт работы: <?php echo "{$result['experience']}"; ?> лет</h11>
+                </div>
+                <div class="doctor_info">
+                    <h12>Дата приёма</h12>
+                        <div class="row_schedule">
+                            <div class="row_schedule_btn">
+                        <?php
+                        $d = new DateTime(); ?>
+                                <input id="date-21.10.2022" type="radio" name="date" value="21.10.2022">
+                                <label for="date-21.10.2022"> <?php echo $d->modify('+1day')->format('d.m.Y'); ?></label>
+                            </div>
+                            <div class="row_schedule_btn">
+                                <input id="date-22.10.2022" type="radio" name="date" value="22.10.2022">
+                                <label for="date-22.10.2022"> <?php echo $d->modify('+1day')->format('d.m.Y'); ?></label>
+                            </div>
+                            <div class="row_schedule_btn">
+                                <input id="date-23.10.2022" type="radio" name="date" value="23.10.2022">
+                                <label for="date-23.10.2022"> <?php echo $d->modify('+1day')->format('d.m.Y'); ?></label>
+                            </div>
+                            <div class="row_schedule_btn">
+                                <input id="date-24.10.2022" type="radio" name="date" value="24.10.2022">
+                                <label for="date-24.10.2022"> <?php echo $d->modify('+1day')->format('d.m.Y'); ?></label>
+                            </div>
+                            <div class="row_schedule_btn">
+                                <input id="date-25.10.2022" type="radio" name="date" value="25.10.2022">
+                                <label for="date-25.10.2022"> <?php echo $d->modify('+1day')->format('d.m.Y'); ?></label>
+                            </div>
                         </div>
-                        <div class="row_schedule_btn">
-                            <input id="date-22.10.2022" type="radio" name="date" value="22.10.2022">
-                            <label for="date-22.10.2022"> <?php echo $d->modify('+1day')->format('d.m.Y'); ?></label>
-                        </div>
-                        <div class="row_schedule_btn">
-                            <input id="date-23.10.2022" type="radio" name="date" value="23.10.2022">
-                            <label for="date-23.10.2022"> <?php echo $d->modify('+1day')->format('d.m.Y'); ?></label>
-                        </div>
-                        <div class="row_schedule_btn">
-                            <input id="date-24.10.2022" type="radio" name="date" value="24.10.2022">
-                            <label for="date-24.10.2022"> <?php echo $d->modify('+1day')->format('d.m.Y'); ?></label>
-                        </div>
-                        <div class="row_schedule_btn">
-                            <input id="date-25.10.2022" type="radio" name="date" value="25.10.2022">
-                            <label for="date-25.10.2022"> <?php echo $d->modify('+1day')->format('d.m.Y'); ?></label>
-                        </div>
-                    </div>
-            </div>
-            <div class="doctor_info">
+                </div>
+                <div class="doctor_info">
                     <h12>Время приёма</h12>
                     <div class="row_schedule">
-
                     <?php
-                          
-                            foreach ($time as $times) {?>
-
+                        foreach ($time as $times) {?>
                         <div class="row_schedule_btn">
                             <input id="time-<?php echo $times['time'];  ?>" type="radio" name="time_reception" value="<?php echo $times['time'];  ?>">
                             <label for="time-<?php echo $times['time'];  ?>" name="time_reception"><?php echo $times['time'];  ?></label>
                         </div>
-                        
                     <?php } ?>
                     </div>
-            </div>
-
-            <div class="button">
-                <input class="form-submit" type="submit" name="zapis" value="Записаться на приём">
-            </div>
-        </div>
-        
+                </div>
+                <div class="button">
+                    <input class="form-submit" type="submit" name="submit" value="Записаться на приём">
+                </div>
+            </form>
+        </div>  
     </main>
-    
     <footer>
         <a class="ft">
             <h3>ООО «SMILE», ИНН 1234567891</h3>
