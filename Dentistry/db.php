23cee0017c9549445e3b95a3187ee42544a151fd
AlyonaShikaeva1 <71886949+AlyonaShikaeva1@users.noreply.github.com>
2022-12-02 20:27:18 +0300

    Add files via upload

--- Dentistry/db.php
+++ Dentistry/db.php
@@ -40,4 +40,42 @@ function get_times($id_doctor){
     $times=$connection->query("SELECT * FROM time_of_doctor WHERE id_doctor=$id_doctor");
     return $times;
     } 
+
+function get_services(){
+global $connection;
+$service=$connection->query("SELECT * FROM services WHERE id_type=1");
+return $service;
+}
+
+function get_services_two(){
+global $connection;
+$service_two=$connection->query("SELECT * FROM services WHERE id_type=2");
+return $service_two;
+}
+
+function get_services_three(){
+global $connection;
+$service_three=$connection->query("SELECT * FROM services WHERE id_type=3");
+return $service_three;
+}
+function get_services_four(){
+global $connection;
+$service_four=$connection->query("SELECT * FROM services WHERE id_type=4");
+return $service_four;
+}
+function get_services_five(){
+global $connection;
+$service_five=$connection->query("SELECT * FROM services WHERE id_type=5");
+return $service_five;
+}
+function get_services_six(){
+global $connection;
+$service_six=$connection->query("SELECT * FROM services WHERE id_type=6");
+return $service_six;
+}
+function get_services_seven(){
+global $connection;
+$service_seven=$connection->query("SELECT * FROM services WHERE id_type=7");
+return $service_seven;
+}
 ?>
