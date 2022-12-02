0e9f63d44166028bfb392fdff09376979e1674cf
AlyonaShikaeva1 <71886949+AlyonaShikaeva1@users.noreply.github.com>
2022-12-02 20:26:52 +0300

    Add files via upload

--- Dentistry/Prices_costs.php
+++ Dentistry/Prices_costs.php
@@ -44,103 +44,76 @@
             <div class="prices_title">
                 <h2>Услуги и цены</h2>
             </div>
+
+            
             <div class="prices">
                 <h5>Диагностика</h5>
+                <?php
+                $services=get_services();
+                foreach ($services as $service) {?>
                 <a class="price_list">
-                    <div class="price_list_title">Прицельный снимок</div>
-                    <div class="cost">400 руб.</div>
+                    <div class="price_list_title"><?php echo $service['name'];?></div>
+                    <div class="cost"><?php echo $service['price'];?></div>
                 </a>
-                <a class="price_list">
-                    <div class="price_list_title">Панорамный снимок (ортопантомограмма)</div>
-                    <div class="cost">1 200 руб.</div>
-                </a>
-                <a class="price_list">
-                    <div class="price_list_title">Консультация стоматолога</div>
-                    <div class="cost">Бесплатно</div>
-                </a>
+                 <?php }?>
                 <h5>Профилактика</h5>
+                <?php
+                $services=get_services_two();
+                foreach ($services as $service) {?>
                 <a class="price_list">
-                    <div class="price_list_title">Удаление зубного камня</div>
-                    <div class="cost">от 150 руб.</div>
+                    <div class="price_list_title"><?php echo $service['name'];?></div>
+                    <div class="cost"><?php echo $service['price'];?></div>
                 </a>
-                <a class="price_list">
-                    <div class="price_list_title">Чистка Air Flow</div>
-                    <div class="cost">от 1 700 руб.</div>
-                </a>
-                <a class="price_list">
-                    <div class="price_list_title">Профессиональная чистка зубов ClinPro</div>
-                    <div class="cost">7 000 руб.</div>
-                </a>
+                <?php }?>
                 <h5>Лечение зубов</h5>
+                <?php
+                $services=get_services_three();
+                foreach ($services as $service) {?>
                 <a class="price_list">
-                    <div class="price_list_title">Пломбирование зубов</div>
-                    <div class="cost">от 6 000 руб.</div>
+                    <div class="price_list_title"><?php echo $service['name'];?></div>
+                    <div class="cost"><?php echo $service['price'];?></div>
                 </a>
-                <a class="price_list">
-                    <div class="price_list_title">Лечение кариеса зубов</div>
-                    <div class="cost">6 000 руб.</div>
-                </a>
-                <a class="price_list">
-                    <div class="price_list_title">Лечение пульпита</div>
-                    <div class="cost">от 15 000 руб.</div>
-                </a>
-                <a class="price_list">
-                    <div class="price_list_title">Лечение корневых каналов</div>
-                    <div class="cost">от 15 000 руб.</div>
-                </a>
+                <?php }?>
+                
                 <h5>Протезирование</h5>
+                 <?php
+                $services=get_services_four();
+                foreach ($services as $service) {?>
                 <a class="price_list">
-                    <div class="price_list_title">Протезирование на имплантах</div>
-                    <div class="cost">от 25 000 руб.</div>
+                    <div class="price_list_title"><?php echo $service['name'];?></div>
+                    <div class="cost"><?php echo $service['price'];?></div>
                         </a>
-                <a class="price_list">
-                    <div class="price_list_title">Несъемное протезирование</div>
-                    <div class="cost">от 1 000 руб.</div>
-                </a>
-                <a class="price_list">
-                    <div class="price_list_title">Съёмное протезирование</div>
-                    <div class="cost">от 2 000 руб.</div>
-                </a>
+                  <?php }?>
                 <h5>Лечение без боли</h5>
+                 <?php
+                $services=get_services_five();
+                foreach ($services as $service) {?>
                 <a class="price_list">
-                    <div class="price_list_title">Лечение зубов под общим наркозом</div>
-                    <div class="cost">от 14 000 руб.</div>
+                    <div class="price_list_title"><?php echo $service['name'];?></div>
+                    <div class="cost"><?php echo $service['price'];?></div>
                 </a>
-                <a class="price_list">
-                    <div class="price_list_title">Лечение зубов с анастезией</div>
-                    <div class="cost">от 250 руб.</div>
-                </a>
-                <a class="price_list">
-                    <div class="price_list_title">Удаление зубов под наркозом</div>
-                    <div class="cost">от 14 000 руб.</div>
-                </a>
+                <?php }?>
                 <h5>Эстетическая стоматология</h5>
+                <?php
+                $services=get_services_six();
+                foreach ($services as $service) {?>
                 <a class="price_list">
-                    <div class="price_list_title">Отбеливание</div>
-                    <div class="cost">20 000 руб.</div>
+                    <div class="price_list_title"><?php echo $service['name'];?></div>
+                    <div class="cost"><?php echo $service['price'];?></div>
                 </a>
-                <a class="price_list">
-                    <div class="price_list_title">Удаление зубного налёта</div>
-                    <div class="cost">1 700 руб.</div>
-                </a>
-                <a class="price_list">
-                    <div class="price_list_title">Реставрация зубов</div>
-                    <div class="cost">7 800 руб.</div>
-                </a>
+               <?php }?>
                 <h5>Исправление прикуса</h5>
+                <?php
+                $services=get_services_seven();
+                foreach ($services as $service) {?>
                 <a class="price_list">
-                    <div class="price_list_title">Установка брекетов</div>
-                    <div class="cost">от 20 000 руб.</div>
+                    <div class="price_list_title"><?php echo $service['name'];?></div>
+                    <div class="cost"><?php echo $service['price'];?></div>
                 </a>
-                <a class="price_list">
-                    <div class="price_list_title">Лингвальные брекеты</div>
-                    <div class="cost">от 11 000 руб.</div>
-                </a>
-                <a class="price_list">
-                    <div class="price_list_title">Ортодонтические микроимпланты</div>
-                    <div class="cost">от 12 500 руб.</div>
-                </a>
-            </div>    
+                <?php }?>
+            </div>   
+           
+ 
         </section>
     </main>
     <footer>
